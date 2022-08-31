88d741d496f0e25780ed6c3e58b8279ce29d60a1
blissdotpy <75507029+blissdotpy@users.noreply.github.com>
2022-08-30 22:11:57 -0500

    impact and sound

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Enemy : MonoBehaviour
@@ -10,14 +12,48 @@ public class Enemy : MonoBehaviour
     [SerializeField] private ParticleSystem _impactParticles;
 
     [SerializeField] private AudioClip _impactSound;
+
+    private Rigidbody _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            PlayerImpact(player);
+            ImpactFeedback();
+        }
+    }
+
+    public void PlayerImpact(Player player)
+    {
+        player.DecreaseHealth(_damageAmount);
+    }
+
+    public void ImpactFeedback()
+    {
+        if (_impactParticles != null)
+        {
+            _impactParticles = Instantiate(_impactParticles, transform.position, Quaternion.identity);
+        }
+
+        if (_impactSound != null)
+        {
+            AudioHelper.PlayClip2D(_impactSound, 1f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
     {
         
     }
